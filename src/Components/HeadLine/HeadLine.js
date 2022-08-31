b7dbde4539dacee87071bd5f0d15549288586856
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-08-31 11:22:13 +0200

    Add parallax

--- src/Components/HeadLine/HeadLine.js
+++ src/Components/HeadLine/HeadLine.js
@@ -1,30 +1,48 @@
-import React from 'react';
-import styles from './headline.module.css';
-import videoBg from '../../assets/stars-headline-bg.mp4';
+import React, { useState, useEffect } from "react";
+import styles from "./headline.module.css";
+import videoBg from "../../assets/stars-headline-bg.mp4";
 
-const HeadLine = () => (
-  <section className={styles['head-section']}>
-    <div className={styles['head-video']}>
-      <video className={styles['head-video__content']} autoPlay loop muted>
-        <source src={videoBg} type="video/mp4" />
-      </video>
-    </div>
+const HeadLine = () => {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => {
+    setOffsetY(window.scrollY);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    <div className={styles['headline-text']}>
-      <div className="u-mt-small">
-        <h1 className={styles['headline-text__heading']}>
-          <span className={styles['headline-text__heading--big']}>
-            Discover the beauty of space
-          </span>
-          <div className="u-mt-small">
-            <span className={styles['headline-text__heading--small']}>
-              The world outside your world
-            </span>
-          </div>
-        </h1>
+  return (
+    <section className={styles["head-section"]}>
+      <div className={styles["head-video"]}>
+        <video className={styles["head-video__content"]} autoPlay loop muted>
+          <source src={videoBg} type="video/mp4" />
+        </video>
       </div>
-    </div>
-  </section>
-);
+
+      <div className={styles["headline-text"]}>
+        <div className="u-mt-small">
+          <h1
+            className={styles["headline-text__heading"]}
+            style={{
+              transform: `translateY(${offsetY * 0.9}px)`,
+            }}
+          >
+            <span className={styles["headline-text__heading--big"]}>
+              Discover the beauty of space
+            </span>
+            <div className="u-mt-small">
+              <span className={styles["headline-text__heading--small"]}>
+                The world outside your world
+              </span>
+            </div>
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default HeadLine;
